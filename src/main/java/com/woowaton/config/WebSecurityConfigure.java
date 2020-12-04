1659535303c8c655851d44fc63b40d211507d127
김영훈 <yhkim1031@woowahan.com>
2020-12-04 12:51:58 +0900

    cors fixed

--- src/main/java/com/woowaton/config/WebSecurityConfigure.java
+++ src/main/java/com/woowaton/config/WebSecurityConfigure.java
@@ -73,7 +73,7 @@ public class WebSecurityConfigure extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception{
-        http.cors().configurationSource(corsConfigurationSource())
+        http.cors()
 //                .headers().frameOptions().disable()
                 .and()
                 .csrf()
